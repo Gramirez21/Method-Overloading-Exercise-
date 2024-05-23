ff7851e5fd48e0ecf643e500d2136ec884896c64
Gramirez21 <r.dannysixty@gmail.com>
2024-05-23 14:03:54 -0700

    method

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace MethodOverloading
+﻿using System.ComponentModel.Design;
+
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            
+            Console.WriteLine(Add(1, 0, true));
+
         }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static  string Add(int num1, int num2, bool isMoney)
+
+        {
+            
+            int i = num1 + num2;
+            if (isMoney == true && i > 1) 
+            {
+                return $"{i} dollars";
+            }
+            else if (isMoney == true &&  i == 1)
+            {
+                return $"{i} dollar";
+            }
+            else
+            {
+                return $"{i}";
+            }
+                
+            
+        }
+
     }
 }
